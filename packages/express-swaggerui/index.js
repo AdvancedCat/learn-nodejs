175645a780b9f3f43203901d1cfe1ffc55c8b225
advancedcat <wshx1938@163.com>
2022-12-03 16:26:14 +0800

    update

--- packages/express-swaggerui/index.js
+++ packages/express-swaggerui/index.js
@@ -4,9 +4,11 @@ import { createRequire } from "module";
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 
+import {add} from './utils.js'
+
 const require = createRequire(import.meta.url);
 const swaggerDocument = require("./swagger.json");
-
+console.log('你好中国', add(1,2))
 /* create an express app and use JSON */
 const app = new express()
 app.use(express.json())
---
+++ packages/express-swaggerui/utils.js
@@ -0,0 +1,4 @@
+
+export const add = function(a, b){
+    return a + b * 2
+}
